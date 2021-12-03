aec2247f7bd43ea9625961e498e3c23c8976d3c2
Eric Satoshi <satoshi.eric@gmail.com>
2021-12-03 17:24:38 -0300

    Update derivando_parabola.py

--- derivando_parabola/derivando_parabola.py
+++ derivando_parabola/derivando_parabola.py
@@ -53,11 +53,13 @@ class Parabola(VGroup):
 
 class DerivandoParabola(Scene):
     def construct(self):
+        self.abertura()
         self.init_ojects()
         self.definicoes_em_objetos()
         self.propriedades_parabola()
         self.mostrar_equacoes()
         self.func_inversa()
+        self.fechamento()
 
     def init_ojects(self):
         class Cores:
@@ -74,10 +76,12 @@ class DerivandoParabola(Scene):
         # Objetos da parábola
         parabola = Parabola()
         eixos = Axes(
-            x_axis_config={'include_ticks': False, 'include_tip': False},
-            y_axis_config={'include_ticks': False, 'include_tip': False},
+            x_axis_config={'include_ticks': False},
+            y_axis_config={'include_ticks': False},
             x_length=5, y_length=5
         )
+        eixos_labels = eixos.get_axis_labels('x', 'y')
+
         r = eixos.get_axes()[1].copy()
         r.shift(parabola.p*LEFT).set_color(Cores.reta_diretriz)
 
@@ -114,7 +118,8 @@ class DerivandoParabola(Scene):
             foco, 
             d_p_foco, 
             ponto, 
-            d_p_reta
+            d_p_reta,
+            eixos_labels
         ).scale(0.8).move_to(4*LEFT + DOWN)
 
         self.parabola = parabola_objs[0]
@@ -125,6 +130,7 @@ class DerivandoParabola(Scene):
         self.dist_foco = parabola_objs[5]
         self.ponto = parabola_objs[6]
         self.dist_reta = parabola_objs[7]
+        self.eixos_labels = parabola_objs[8]
 
         # Textos de definição dos objetos da parábola: MathTex
         definicoes = Tex(
@@ -173,6 +179,7 @@ class DerivandoParabola(Scene):
             '2xp + 2xp = y^2',
             'y^2 = 2xp + 2xp',
             'y^2 = 4xp',
+            'x=\\frac{1}{4p}y^2'
         ]
 
         self.eqs = [
@@ -182,54 +189,52 @@ class DerivandoParabola(Scene):
 
         self.desc_func_inversa = Tex('Se usarmos a função inversa, teremos a \\\\ parábola com a concavidade para cima')
         self.v_parabola = Parabola(vertical=True)
-        self.eq_parabola_inversa = MathTex('x^2 = 4yp')
         self.resultado = MathTex('y = \\frac{x^2}{4p}')
 
         self.desc_func_inversa.scale(self.escala_texto).move_to(RIGHT + UP)
         self.v_parabola.scale(0.5).move_to(RIGHT + 0.75*DOWN)
-        self.eq_parabola_inversa.scale(self.escala_texto).move_to(RIGHT + 2*DOWN)
         self.resultado.scale(self.escala_texto).move_to(RIGHT + 2*DOWN)
 
     def definicoes_em_objetos(self):
         play = lambda *anim, t=1: self.play(*anim, run_time=t)
 
-        play(Write(self.eixos), t=1.5)
-        self.wait()
-        play(Write(self.parabola), t=1.5)
-        self.wait()
-        play(Write(self.def_reta_diretriz), t=1.5)
-        self.wait()
-        play(ReplacementTransform(self.def_reta_diretriz.copy(), self.reta_diretriz), t=1.5)
-        self.wait()
-        play(Write(self.def_foco), t=1.5)
-        self.wait()
-        play(ReplacementTransform(self.def_foco.copy(), self.foco), t=1.5)
-        self.wait()
-        play(Write(self.def_parametro), t=1.5)
-        self.wait()
-        play(ReplacementTransform(self.def_parametro.copy(), self.parametro), t=1.5)
-        self.wait()
-        play(Write(self.dist_foco), t=1.5)
-        self.wait()
-        play(Write(self.ponto), t=1.5)
-        self.wait()
-        play(Write(self.dist_reta), t=1.5)
-        self.wait()
+        play(Write(self.eixos), Write(self.eixos_labels), t=2)
+        self.wait(2)
+        play(Write(self.parabola), t=2)
+        self.wait(2)
+        play(Write(self.def_reta_diretriz), t=2)
+        self.wait(2)
+        play(ReplacementTransform(self.def_reta_diretriz.copy(), self.reta_diretriz), t=2)
+        self.wait(2)
+        play(Write(self.def_foco), t=2)
+        self.wait(2)
+        play(ReplacementTransform(self.def_foco.copy(), self.foco), t=2)
+        self.wait(2)
+        play(Write(self.def_parametro), t=2)
+        self.wait(2)
+        play(ReplacementTransform(self.def_parametro.copy(), self.parametro), t=2)
+        self.wait(2)
+        play(Write(self.dist_foco), t=2)
+        self.wait(2)
+        play(Write(self.ponto), t=2)
+        self.wait(2)
+        play(Write(self.dist_reta), t=2)
+        self.wait(2)
 
 
     def propriedades_parabola(self):
         play = lambda *anim, t=1: self.play(*anim, run_time=t)
 
-        play(Write(self.prop_foco), t=1.5)
-        self.wait()
-        play(Write(self.prop_ponto), t=1.5)
-        self.wait()
-        play(Write(self.prop_dist_reta), t=1.5)
-        self.wait()
-        play(Write(self.prop_dist_foco), t=1.5)
-        self.wait()
-        play(Write(self.eq_parabola), t=1.5)
-        self.wait()
+        play(Write(self.prop_foco), t=2)
+        self.wait(2)
+        play(Write(self.prop_ponto), t=2)
+        self.wait(2)
+        play(Write(self.prop_dist_reta), t=2)
+        self.wait(2)
+        play(Write(self.prop_dist_foco), t=2)
+        self.wait(2)
+        play(Write(self.eq_parabola), t=2)
+        self.wait(2)
 
     def mostrar_equacoes(self):
         play = lambda *anim, t=1: self.play(*anim, run_time=t)
@@ -237,15 +242,15 @@ class DerivandoParabola(Scene):
 
         for i in range(len(self.eqs) - 1):
             if i % 4 == 3:
-                play(FadeOut(self.eqs[i - 3]), t=1.5)
-                self.wait()
+                play(FadeOut(self.eqs[i - 3]), t=2)
+                self.wait(2)
             if i % 4 == 0 and i != 0:
-                play(FadeOut(*self.eqs[i-3:i]), t=1.5)
-                self.wait()
-            play(ReplacementTransform(self.eqs[i].copy(), self.eqs[i+1]), t=1.5)
-            self.wait()
+                play(FadeOut(*self.eqs[i-3:i]), t=2)
+                self.wait(2)
+            play(ReplacementTransform(self.eqs[i].copy(), self.eqs[i+1]), t=2)
+            self.wait(2)
 
-        play(FadeOut(*self.eqs[-4:-1]), t=1.5)
+        play(FadeOut(*self.eqs[-4:-1]), t=2)
         play(Write(destaque := SurroundingRectangle(self.eqs[-1])), t=2)
         self.wait()
 
@@ -254,16 +259,41 @@ class DerivandoParabola(Scene):
     def func_inversa(self):
         play = lambda *anim, t=1: self.play(*anim, run_time=t)
 
-        play(Write(self.desc_func_inversa), t=1.5)
+        play(Write(self.desc_func_inversa), t=2)
+        self.wait(2)
+        play(Write(self.v_parabola), t=2)
+        self.wait(2)
+        play(Write(self.resultado), t=2)
+        self.wait(2)
+        play(Write(SurroundingRectangle(self.resultado)), t=2)
+        self.wait(2)
+
+        play(FadeOut(*[mob for mob in self.mobjects]), t=2)
+
+    def abertura(self):
+        titulo = Tex('A Equação da Parábola').scale(2.5).set_color("#dc6a40").move_to(0.5*UP)
+        self.play(FadeIn(titulo))
+        self.wait(1.5)
+        self.play(FadeOut(titulo))
         self.wait()
-        play(Write(self.v_parabola), t=1.5)
-        self.wait()
-        play(Write(self.eq_parabola_inversa), t=1.5)
-        self.wait()
-        play(ReplacementTransform(self.eq_parabola_inversa, self.resultado), t=1.5)
-        self.wait()
-        play(Write(SurroundingRectangle(self.resultado)), t=1.5)
-        self.wait()
+
+    def fechamento(self):
+        pibit = MathTex("\\text{PIBIT/CNPQ: 0220036212472856}").scale(1.5).move_to(2*UP).set_color(DARK_BLUE)
+        autor = MathTex("\\text{Autor: Eric Satoshi Suzuki Kishimoto}").set_color("#dc6a40").move_to(ORIGIN)
+        orientador = MathTex("\\text{Orientador: Prof. Vitor Rafael Coluci}").set_color("#dc6a40").move_to(DOWN)
+        ft = ImageMobject("./logo-FT.jpeg").scale(0.4).shift(1.5*DOWN+3*RIGHT)
+        unicamp = ImageMobject("./logo-unicamp.jpeg").scale(0.3).shift(1.5*DOWN+3*LEFT)
+
+        self.play(FadeIn(pibit))
+        self.wait(1)
+        self.play(FadeIn(unicamp), FadeIn(ft))
+        self.wait(1)
+        self.play(FadeOut(unicamp), FadeOut(ft))
+        self.wait(0.8)
+        self.play(FadeIn(autor), FadeIn(orientador))
+        self.wait(2)
+        self.play(FadeOut(*[mob for mob in self.mobjects]))
+        self.wait(2)
 
 ARQ_NOME = Path(__file__).resolve()
 CENA = DerivandoParabola.__name__
