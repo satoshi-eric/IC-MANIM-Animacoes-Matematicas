7f8a7f19dec359f0c572e463d4438e1d8b1a7e4a
Eric Satoshi <satoshi.eric@gmail.com>
2022-01-05 15:42:32 -0300

    Função Polinomial adicionada

---
+++ funcao_polinomial/funcao_polinomial.py
@@ -0,0 +1,32 @@
+from manim import *
+from pathlib import Path
+import os
+from typing import List
+
+class SistemaEquacoes(VGroup):
+    def __init__(self, equacoes: List[str], **kwargs):
+        super().__init__()
+        str_equacoes = list(map(lambda eq: r"\text{\raggedright} " + eq + " \\", equacoes))
+        m_equacoes = MathTex(*str_equacoes)
+        brace = Brace(m_equacoes, LEFT)
+        self.equacoes = m_equacoes
+        self.brace = brace
+        self.add(m_equacoes, brace)
+
+    def __getitem__(self, value):
+        return self.equacoes[value]
+        
+
+class FuncaoPolinomial(Scene):
+    def construct(self):
+        sis = SistemaEquacoes(["{{x^2}} + {{2x}} + {{1}} = {{0}}", "{{x^2}} - {{2x}} + {{1}} = {{0}}"])
+        eq = MathTex("\\text{\\raggedright} {{ x^2 }} + {{ 2x }} + {{ 1 }} = {{ 1 }} \\\\", "\\text{\\raggedright} {{ x^2 }} - {{ 2x }} + {{ 1 }} = {{ 1 }}")
+        self.play(Write(eq))
+
+
+ARQ_NOME = Path(__file__).resolve()
+CENA = FuncaoPolinomial.__name__
+ARGS = '-s'
+
+if __name__ == '__main__':
+    os.system(f'manim {ARQ_NOME} {CENA} {ARGS}')
