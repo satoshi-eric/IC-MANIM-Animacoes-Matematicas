fdd3224ad35fe30b686ad613cd3d177853dc75d7
Eric Satoshi <satoshi.eric@gmail.com>
2022-01-05 15:52:09 -0300

    Update funcao_polinomial.py

--- funcao_polinomial/funcao_polinomial.py
+++ funcao_polinomial/funcao_polinomial.py
@@ -5,6 +5,7 @@ from typing import List
 
 class SistemaEquacoes(VGroup):
     def __init__(self, equacoes: List[str], **kwargs):
+<<<<<<< Updated upstream
         super().__init__()
         str_equacoes = list(map(lambda eq: r"\text{\raggedright} " + eq + " \\", equacoes))
         m_equacoes = MathTex(*str_equacoes)
@@ -15,14 +16,22 @@ class SistemaEquacoes(VGroup):
 
     def __getitem__(self, value):
         return self.equacoes[value]
+=======
+        super().__init__(**kwargs)
+        equacoes = MathTex(map(lambda eq: ""))
+>>>>>>> Stashed changes
         
 
 class FuncaoPolinomial(Scene):
     def construct(self):
+<<<<<<< Updated upstream
         sis = SistemaEquacoes(["{{x^2}} + {{2x}} + {{1}} = {{0}}", "{{x^2}} - {{2x}} + {{1}} = {{0}}"])
         eq = MathTex("\\text{\\raggedright} {{ x^2 }} + {{ 2x }} + {{ 1 }} = {{ 1 }} \\\\", "\\text{\\raggedright} {{ x^2 }} - {{ 2x }} + {{ 1 }} = {{ 1 }}")
         self.play(Write(eq))
 
+=======
+        pass
+>>>>>>> Stashed changes
 
 ARQ_NOME = Path(__file__).resolve()
 CENA = FuncaoPolinomial.__name__
