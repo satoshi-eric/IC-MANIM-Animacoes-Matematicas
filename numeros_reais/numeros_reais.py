7045509b3675bb28aebb0eebe04a53d240b1c421
Eric Satoshi <satoshi.eric@gmail.com>
2022-04-12 23:06:37 -0300

    Update numeros_reais.py

--- numeros_reais/numeros_reais.py
+++ numeros_reais/numeros_reais.py
@@ -33,16 +33,16 @@ class NumerosReais(Scene):
         introducao = Tex('\\raggedright Podemos definir os números \
              reais como o conjunto no qual qualquer \
              número inteiro ou decimal pertence a esse grupo.'
-        ).scale(0.5).to_edge(UP)
+        ).scale(0.7).to_edge(UP)
 
         conjunto = VDict({
             'diagraama': Ellipse(2, 4),
             'numeros': VGroup(
-                MathTex('1'),
-                MathTex('2'),
-                MathTex('2,71'),
-                MathTex('3'),
-                MathTex('3,14')
+                MathTex('1').scale(0.8),
+                MathTex('2').scale(0.8),
+                MathTex('2,71').scale(0.8),
+                MathTex('3').scale(0.8),
+                MathTex('3,14').scale(0.8),
             ).arrange(RIGHT, buff=0.5).shift(4*RIGHT),
         }, show_keys=False).shift(4*LEFT)
 
@@ -53,46 +53,76 @@ class NumerosReais(Scene):
             play(
                 MoveAlongPath(
                     conjunto['numeros'][i], 
-                    ArcBetweenPoints(conjunto['numeros'][i].get_center(), 4*LEFT + 1.5*UP + 0.5*DOWN*i)
+                    ArcBetweenPoints(conjunto['numeros'][i].get_center(), 4*LEFT + 1.5*UP + 0.75*DOWN*i)
                 ),
             )
 
+        self.clear_scene()
+
     def definicao_2(self):
-        introducao = Tex('\\raggedright O conjunto do reais pode ser definido de outra forma, a união entre os conjuntos dos racionais e irracionais')
+        def play(*anim, anim_time=2, wait_time=2):
+            self.play(*anim, run_time=anim_time)
+            self.wait(wait_time)
 
-        conjunto_racionais = Tex('O conjunto dos racionais se refere ao conjunto dos números que podem ser representados por uma fração')
-        racionais_exemplo = MathTex(r'\frac{12}{10} = 1,2 = 1 + 0,2')
+        introducao = Tex(
+            '\\raggedright O conjunto do reais pode ser definido de outra forma, a união entre os conjuntos dos racionais e irracionais'
+        ).scale(0.7).to_edge(UP)
 
-        conjunto_irracionais = Tex('O conjunto dos irracionais se refere ao conjunto dos números com casas decimais, mas não podem ser representados como fração.')
-        irracionais_exemplo = MathTex(r'\pi = 3,141592653589793 \\ e = 2,718281828459045')
+        conjunto_racionais = Tex(
+            '\\raggedright O conjunto dos racionais se refere ao conjunto dos números que podem ser representados por uma fração'
+        ).scale(0.7).to_edge(UP)
+        racionais_exemplo = MathTex(
+            r'\frac{12}{10} = 1,2 = 1 + 0,2'
+        ).scale(0.7).next_to(conjunto_racionais, DOWN)
 
-        diagrama_texto = Tex('Dessa forma, temos o seguinte diagrama de Venn')
+        conjunto_irracionais = Tex(
+            '\\raggedright O conjunto dos irracionais se refere ao conjunto dos números com casas decimais, mas não podem ser representados como fração.'
+        ).scale(0.7).to_edge(UP)
+        irracionais_exemplo = MathTex(
+            r'\pi = 3,141592653589793... \\ e = 2,718281828459045...'
+        ).scale(0.7).next_to(conjunto_irracionais, DOWN)
+
+        diagrama_texto = Tex(
+            'Dessa forma, temos o seguinte diagrama de Venn'
+        ).scale(0.7).to_edge(UP)
         diagrama_venn = VDict({
             'reais': VDict({
-                'diagrama': Ellipse(2, 4),
-                'simbolo': Tex(r'\mathbb{R}'),
+                'diagrama': Ellipse(4, 5),
+                'simbolo': MathTex(r'\mathbb{R}'),
             }),
             'racionais': VDict({
-                'diagrama': Ellipse(2, 3),
-                'simbolo': Tex(r'\mathbb{Q}')
+                'diagrama': Ellipse(2, 3).shift(2*RIGHT),
+                'simbolo': MathTex(r'\mathbb{Q}')
             }),
             'irracionais': VDict({
-                'diagrama': Ellipse(2, 2),
-                'simbolo': Tex(r'\mathbb{Z}')
+                'diagrama': Ellipse(2, 2).shift(2*LEFT),
+                'simbolo': MathTex(r'\mathbb{Z}')
             }),
-            'inteiros': VDict({
-                'diagrama': Ellipse(2, 2),
-                'simbolo': Tex(r'\mathbb{Z}')
-            }),
-            'naturais': VDict({
-                'diagrama': Ellipse(2, 1),
-                'simbolo': Tex(r'\mathbb{N}')
-            })
         })
+
+        play(Write(introducao))
+        play(FadeOut(introducao))
+        play(Write(conjunto_racionais))
+        play(FadeOut(conjunto_racionais))
+        play(Write(racionais_exemplo))
+        play(FadeOut(racionais_exemplo))
+        play(Write(conjunto_irracionais))
+        play(FadeOut(conjunto_irracionais))
+        play(Write(irracionais_exemplo))
+        play(FadeOut(irracionais_exemplo))
+        play(Write(diagrama_texto))
+        play(Write(diagrama_venn['racionais']))
+        play(Write(diagrama_venn['irracionais']))
+        play(Write(diagrama_venn['reais']))
+        # play(FadeOut(diagrama_texto))
+        # play(FadeOut(diagrama_venn))
+
+    def clear_scene(self):
+        self.play(FadeOut(*[mob for mob in self.mobjects if type(mob) != Grade]))
 
 ARQ_NOME = Path(__file__).resolve()
 CENA = NumerosReais.__name__
-ARGS = '-pql'
+ARGS = '-s'
 
 if __name__ == '__main__':
     os.system(f'manim {ARQ_NOME} {CENA} {ARGS}')
